abb0a599c6cf2802abe16e7777393b8f760da81f
DEVEN WAGH <wagh1.2.3.002@gmail.com>
2025-04-09 21:46:43 +0530

    Fix logo image path in Logo component; update build scripts and add Vercel configuration

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link to="/" className="flex items-center p-2 w-30 ">
       <img
-        src="./action.svg"
+        src="/action.svg"
         alt="WatchWise"
         className="w-auto h-30"
       />
