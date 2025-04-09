6399232ace49e89e5e3463a1ec351bc10fc72ff2
DEVEN WAGH <wagh1.2.3.002@gmail.com>
2025-04-09 23:36:28 +0530

    chore: update dependencies and remove unused files
    - Updated various dependencies in package.json to their latest versions, including @react-router, @reduxjs/toolkit, axios, and others.
    - Removed react-router.config.ts as it is no longer needed.
    - Deleted vercel.json configuration file.
    - Cleaned up vite.config.ts by adjusting plugin imports and formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    outDir: 'build',
-    sourcemap: true
-  },
-  server: {
-    host: '0.0.0.0'
-  }
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
